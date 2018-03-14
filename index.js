090991b002e7ec39c22a33d2159eec63309e0b50
William J Teskey <willjteskey@gmail.com>
2018-03-14 22:16:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,9 +6,7 @@ function whisper(string) {
   return string.toLowerCase()
 }
 
-function sayHiToGrandma(string) {
-  console.log(toUpperCase, ${'I can/t hear you}!`)
-}
+
 
 function sayHiToGrandma() {
   console.log("I can/t hear you!")
